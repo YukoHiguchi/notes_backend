ebbeae549d525c6ab3850fa0e6bfaa19ed42ec3a
Yuko Higuchi <39221794+YukoHiguchi@users.noreply.github.com>
2023-12-28 00:05:08 -0500

    add static files to the backend

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
+const cors = require("cors");
 
-app.use(express.json());
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
 app.use(cors());
+app.use(express.json());
+app.use(requestLogger);
+app.use(express.static("build"));
+app.use(express.static("dist"));
 
 let notes = [
   {
@@ -66,6 +81,8 @@ app.get("/api/notes/:id", (request, response) => {
   } else {
     response.status(404).end();
   }
+
+  response.json(note);
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -74,6 +91,8 @@ app.delete("/api/notes/:id", (request, response) => {
 
   response.status(204).end();
 });
+
+app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
